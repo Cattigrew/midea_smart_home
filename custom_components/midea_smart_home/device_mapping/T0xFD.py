68dc7d3d005f7f75c4aeb0b6cac8b771551b749d
Cyborg2017 <wangyang0987654@gmail.com>
2026-03-17 09:37:36 +0800

    feat: add support for new T0xFD device(202Z3119)

--- custom_components/midea_smart_home/device_mapping/T0xFD.py
+++ custom_components/midea_smart_home/device_mapping/T0xFD.py
@@ -58,5 +58,71 @@ DEVICE_MAPPING = {
                 }
             }
         }
+    },
+    "202Z3119": {
+        "rationale": ["off", "on"],
+        "entities": {
+            Platform.SWITCH: {
+                "buzzer": {
+                    "device_class": SwitchDeviceClass.SWITCH,
+                },
+                "airDry_on_off": {
+                    "device_class": SwitchDeviceClass.SWITCH,
+                    "translation_key": "airdry_on_off"
+                },
+                "power": {
+                    "device_class": SwitchDeviceClass.SWITCH,
+                },
+                "light_color": {
+                    "device_class": SwitchDeviceClass.SWITCH,
+                    "rationale": ["off", "warm"]
+                }
+            },
+            Platform.SELECT: {
+                "humidity_mode": {
+                    "options": {
+                        "manual": {"humidity_mode": "manual"},
+                        "moist_skin": {"humidity_mode": "moist_skin"},
+                        "sleep": {"humidity_mode": "sleep"}
+                    }
+                },
+                "wind_speed": {
+                    "options": {
+                        "low": {"wind_speed": "low"},
+                        "middle": {"wind_speed": "middle"},
+                        "high": {"wind_speed": "high"}
+                    }
+                },
+                "bright_led": {
+                    "options": {
+                        "exit": {"bright_led": "exit"},
+                        "dark": {"bright_led": "dark"},
+                        "light": {"bright_led": "light"}
+                    }
+                }
+            },
+            Platform.SENSOR: {
+                "cur_temperature": {
+                    "device_class": SensorDeviceClass.TEMPERATURE,
+                    "unit_of_measurement": UnitOfTemperature.CELSIUS,
+                    "state_class": SensorStateClass.MEASUREMENT
+                },
+                "cur_humidity": {
+                    "device_class": SensorDeviceClass.HUMIDITY,
+                    "unit_of_measurement": PERCENTAGE,
+                    "state_class": SensorStateClass.MEASUREMENT
+                },
+                "air_dry_left_time": {
+                    "device_class": SensorDeviceClass.DURATION,
+                    "unit_of_measurement": UnitOfTime.SECONDS,
+                    "state_class": SensorStateClass.MEASUREMENT
+                },
+                "tank_status": {
+                    "device_class": SensorDeviceClass.BATTERY,
+                    "unit_of_measurement": PERCENTAGE,
+                    "state_class": SensorStateClass.MEASUREMENT
+                }
+            }
+        }
     }
 }
