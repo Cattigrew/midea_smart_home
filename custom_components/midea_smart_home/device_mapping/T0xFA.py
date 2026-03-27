84f2cd0da56e3c7d533d8d41cdf5a99f1174a273
Cyborg2017 <wangyang0987654@gmail.com>
2026-03-27 13:51:09 +0800

    feat: add support for new T0xFA device(BGF10000). fix: https://github.com/Cyborg2017/midea_smart_home/issues/16

--- custom_components/midea_smart_home/device_mapping/T0xFA.py
+++ custom_components/midea_smart_home/device_mapping/T0xFA.py
@@ -46,5 +46,87 @@ DEVICE_MAPPING = {
                 }
             }
         }
+    },
+    "default": {
+        "rationale": ["off", "on"],
+        "entities": {
+            Platform.SWITCH: {
+                "display_on_off": {
+                    "device_class": SwitchDeviceClass.SWITCH,
+                    "rationale": ["on", "off"],
+                },
+                "humidify": {
+                    "device_class": SwitchDeviceClass.SWITCH,
+                    "rationale": ["off", "1"],
+                },
+                "waterions": {
+                    "device_class": SwitchDeviceClass.SWITCH,
+                }
+            },
+            Platform.FAN: {
+                "fan": {
+                    "power": "power",
+                    "speeds": list({"gear": value + 1} for value in range(0, 12)),
+                    "oscillate": "swing",
+                    "preset_modes": {
+                        "normal": {
+                            "mode": "normal",
+                            "speeds": list({"gear": value + 1} for value in range(0, 12))
+                        },
+                        "comfort": {
+                            "mode": "comfort",
+                            "speeds": list({"gear": value + 1} for value in range(0, 3))
+                        },
+                        "sleep": {
+                            "mode": "sleep",
+                            "speeds": list({"gear": value + 1} for value in range(0, 3))
+                        },
+                        "baby": {
+                            "mode": "baby",
+                            "speeds": list({"gear": value + 1} for value in range(0, 1))
+                        },
+                        "strong": {
+                            "mode": "strong",
+                            "speeds": [{"gear": 12}]
+                        }
+                    }
+                }
+            },
+            Platform.SENSOR: {
+                "water_feedback": {
+                    "device_class": SensorDeviceClass.ENUM
+                }
+            }
+        }
+    },
+    "BGF10000": {
+        "rationale": ["off", "on"],
+        "entities": {
+            Platform.FAN: {
+                "fan": {
+                    "power": "power",
+                    "speeds": list({"gear": value + 1} for value in range(0, 12)),
+                    "oscillate": "swing",
+                    "preset_modes": {
+                        "normal": {
+                            "mode": "normal",
+                            "speeds": list({"gear": value + 1} for value in range(0, 12))
+                        },
+                        "comfort": {
+                            "mode": "comfort",
+                            "speeds": list({"gear": value + 1} for value in range(0, 3))
+                        },
+                        "sleep": {
+                            "mode": "sleep",
+                            "speeds": list({"gear": value + 1} for value in range(0, 3))
+                        },
+                        "strong": {
+                            "mode": "strong",
+                            "speeds": [{"gear": 12}]
+                        }
+                    }
+                }
+            }
+        }
     }
 }
